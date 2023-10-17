c6b0af99a9ca823326c12c799ad17dabde3d57e9
Dharma1794 <dharmarajblue7@gmail.com>
2023-10-17 18:55:36 +0530

    assessment done

---
+++ src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,39 @@
+package com.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.utility.SeleniumCommands;
+
+public class LoginPage extends SeleniumCommands{
+	
+	
+	@FindBy(name="username")
+	private WebElement username;
+	
+	@FindBy(name="password")
+	private WebElement password;
+	
+	@FindBy(xpath="//button[@type='submit']")
+	private WebElement login;
+
+	public WebElement getUsername() {
+		return username;
+	}
+
+	public WebElement getPassword() {
+		return password;
+	}
+
+	public WebElement getLogin() {
+		return login;
+	}
+	
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+
+}
